b2d18249077b16687b1f25aad3864f120bc0bb6e
TomDarmon <tom.darmon@artefact.com>
2023-11-06 17:49:57 +0100

    fix: data folder + script

---
+++ bin/download_sample_sequences.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+N_SEQUENCES=5
+
+sequences_detections=$(gsutil ls gs://data-track-reid/detections | head -$N_SEQUENCES)
+sequences_frames=$(gsutil ls gs://data-track-reid/frames | head -$N_SEQUENCES)
+
+# remove first sequence which is the bucket name
+sequences_detections=$(echo "$sequences_detections" | tail -n +2)
+sequences_frames=$(echo "$sequences_frames" | tail -n +2)
+
+
+# # download the sequences to data/detections and data/frames
+# for sequence in $sequences_detections; do
+#     gsutil -m cp -r $sequence data/detections
+# done
+
+for sequence in $sequences_frames; do
+    gsutil -m cp -r $sequence data/frames
+done
