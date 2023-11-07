2e1c7340558ffd4d9b7dab68e60e9f38423dd913
tristanpepinartefact <tristan.pepin@artefact.com>
2023-11-07 11:08:37 +0100

    add tracked object filter

---
+++ track_reid/tracked_object_filter.py
@@ -0,0 +1,18 @@
+from track_reid.constants.reid_constants import reid_constants
+
+
+class TrackedObjectFilter:
+    def __init__(self, confidence_threshold, frames_seen_threshold):
+        self.confidence_threshold = confidence_threshold
+        self.frames_seen_threshold = frames_seen_threshold
+
+    def update(self, tracked_object):
+        if tracked_object.get_state() == reid_constants.BYETRACK_OUTPUT:
+            if (
+                tracked_object.metadata.mean_confidence() > self.confidence_threshold
+                and tracked_object.metadata.observations >= self.frames_seen_threshold
+            ):
+                tracked_object.state = reid_constants.FILTERED_OUTPUT
+
+        elif tracked_object.metadata.mean_confidence() < self.confidence_threshold:
+            tracked_object.state = reid_constants.BYETRACK_OUTPUT
