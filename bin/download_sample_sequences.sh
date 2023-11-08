4b441d796d2fcb665fb984fd569fd9ac30ef1eac
tristanpepinartefact <tristan.pepin@artefact.com>
2023-11-08 18:37:38 +0100

    add security for new repos

--- bin/download_sample_sequences.sh
+++ bin/download_sample_sequences.sh
@@ -9,6 +9,8 @@ sequences_frames=$(gsutil ls gs://data-track-reid/frames | head -$N_SEQUENCES)
 sequences_detections=$(echo "$sequences_detections" | tail -n +2)
 sequences_frames=$(echo "$sequences_frames" | tail -n +2)
 
+mkdir -p data/detections
+mkdir -p data/frames
 
 # download the sequences to data/detections and data/frames
 for sequence in $sequences_detections; do
