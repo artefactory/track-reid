3b459779361df4f58c501cf971ecb7274e3f5f93
tristanpepinartefact <tristan.pepin@artefact.com>
2023-11-10 13:19:43 +0100

    add constants

--- track_reid/constants/reid_constants.py
+++ track_reid/constants/reid_constants.py
@@ -4,14 +4,16 @@ from pydantic import BaseModel
 
 
 class ReidConstants(BaseModel):
-    BYETRACK_OUTPUT: int = -2
+    LOST_FOREVER: int = -3
+    TRACKER_OUTPUT: int = -2
     FILTERED_OUTPUT: int = -1
     STABLE: int = 0
     SWITCHER: int = 1
     CANDIDATE: int = 2
 
     DESCRIPTION: ClassVar[dict] = {
-        BYETRACK_OUTPUT: "bytetrack output not in reid process",
+        LOST_FOREVER: "switcher never rematched",
+        TRACKER_OUTPUT: "bytetrack output not in reid process",
         FILTERED_OUTPUT: "bytetrack output entering reid process",
         STABLE: "stable object",
         SWITCHER: "lost object to be re-matched",
