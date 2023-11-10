0815bde7c94e968fa33f7338f2610aca83d02f46
tristanpepinartefact <tristan.pepin@artefact.com>
2023-11-10 13:23:52 +0100

    adding reid args

--- track_reid/args/reid_args.py
+++ track_reid/args/reid_args.py
@@ -1,2 +1,11 @@
 POSSIBLE_CLASSES = [0.0, 1.0]
+MAPPING_CLASSES = {0.0: "shop_item", 1.0: "personal_item"}
 # POSSIBLE_CLASSES = ["car", "person", "truck", "animal"]
+
+INPUT_POSITIONS = {
+    "frame_id": 0,
+    "object_id": 1,
+    "object_class": 2,
+    "bbox": [2, 3, 4, 5],
+    "confidence": 6,
+}
