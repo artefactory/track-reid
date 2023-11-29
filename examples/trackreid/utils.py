3d5fedcfe80199ee5cb2e53f269605526fdffe78
TomDarmon <tom.darmon@artefact.com>
2023-11-29 16:12:51 +0100

    fix: working notebook

--- examples/trackreid/utils.py
+++ examples/trackreid/utils.py
@@ -14,11 +14,12 @@ def draw_all_bbox_on_image(image, tracking_objects: np.ndarray):
     Plot this on the image with the track id, class id and confidence.
     """
     for detection in tracking_objects:
-        x1, y1, x2, y2, track_id, class_id, conf = detection
+        x1, y1, x2, y2, track_id, _, conf = detection
+        x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2)
         cv2.rectangle(image, (x1, y1), (x2, y2), (255, 0, 0), 2)
         cv2.putText(
             image,
-            f"ID: {track_id}, Class: {class_id}, Conf: {conf}",
+            f"{int(track_id)} ({conf:.2f})",
             (x1, y1 - 10),
             0,
             1,
@@ -26,45 +27,6 @@ def draw_all_bbox_on_image(image, tracking_objects: np.ndarray):
             2,
         )
     return image
-
-
-def yolo_results_with_track_id(detections, tracking_output: np.ndarray):
-    """Transforms YOLO detections and tracking info into easy to plot format.
-
-    Args:
-        detections: A list of YOLO detections.
-        tracking_results: The output of tracking with format [x1, y1, x2, y2, track_id, class_id, conf].
-
-
-    Returns:
-        A list of of detections with track id and class.
-            [
-                [x, y, x, y, track_id, class, conf],
-                [x, y, x, y, track_id, class, conf],
-                ...
-            ]
-    """
-    # Check if all arrays are the same length
-    if len(detections) != len(tracking_output):
-        raise ValueError("Length of detections and tracking_output must be the same.")
-
-    if len(tracking_output) == 0:
-        return np.array([])
-    boxes = detections.numpy().boxes.xyxy
-    scores = detections.numpy().boxes.conf
-    classes = detections.numpy().boxes.cls
-    return np.stack(
-        [
-            boxes[:, 0],
-            boxes[:, 1],
-            boxes[:, 2],
-            boxes[:, 3],
-            tracking_output[:, 4],
-            classes,
-            scores,
-        ],
-        axis=1,
-    )
 
 
 def yolo_results_to_bytetrack_format(detections):
@@ -83,10 +45,26 @@ def yolo_results_to_bytetrack_format(detections):
         [
             boxes[:, 0],
             boxes[:, 1],
-            boxes[:, 2] - boxes[:, 0],
-            boxes[:, 3] - boxes[:, 1],
+            boxes[:, 2],
+            boxes[:, 3],
             scores,
             classes,
         ],
         axis=1,
     )
+
+
+def scale_bbox_as_xyxy(bbox: np.ndarray, target_img_size: tuple):
+    """Scales a bounding box to a target image size.
+
+    Args:
+        bbox: A bounding box in the format [x, y, x, y].
+        target_img_size: The target image size as a tuple (h, W).
+
+    Returns:
+        The scaled bounding box.
+    """
+    x1, y1, x2, y2 = bbox
+    h, w = target_img_size
+    scaled_bbox = np.array([x1 * w, y1 * h, x2 * w, y2 * h])
+    return scaled_bbox
